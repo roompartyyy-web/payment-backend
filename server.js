f2fc782bcfbc1e61c30237474fe99fab3b08e66d
roompartyyy-web <roompartyyy@gmail.com>
2026-05-25 13:50:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,23 +6,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const API_KEY = process.env.TRANSAK_API_KEY;
+
+const WALLET_ADDRESS = "TON_WALLET_USDT";
+
 app.post("/create-payment", async (req, res) => {
 
   const amount = req.body.amount;
 
-  let paymentUrl = "";
-
-  if (amount == 250) {
-    paymentUrl = "https://example.com/250";
-  }
-
-  if (amount == 500) {
-    paymentUrl = "https://example.com/500";
-  }
-
-  if (amount == 999) {
-    paymentUrl = "https://example.com/999";
-  }
+  const paymentUrl =
+    `https://global.transak.com/?apiKey=${API_KEY}` +
+    `&cryptoCurrencyCode=USDT` +
+    `&network=ethereum` +
+    `&walletAddress=${WALLET_ADDRESS}` +
+    `&fiatCurrency=EUR` +
+    `&fiatAmount=${amount}` +
+    `&redirectURL=https://TONSITE.com/success`;
 
   res.json({
     url: paymentUrl
